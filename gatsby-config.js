b1206dfafd69a303ebab820963942cd63cad00bf
Mohamad Zain Aziz <zaziz@wwf.sg>
2020-02-16 03:46:43 +0800

    removed src/data

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,13 +45,6 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
     `gatsby-plugin-sass`,
-    `gatsby-transformer-json`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`,
-      },
-    },
+    `gatsby-transformer-json`
   ],
 }
